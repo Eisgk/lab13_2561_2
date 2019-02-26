644b626b391530a1ceecad4f21145aafdafa8e17
Eisgk <game.080.pf@gmail.com>
2019-02-26 16:44:50 +0700

    no.1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -2,18 +2,38 @@
 #include <string>
 using namespace std;
 
+int pos;
 template <typename T>
 void swap(T d[],int x,int y){
     T temp = d[x];
     d[x] = d[y];
     d[y] = temp;
+	pos = x;
 }
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
-//Write your code here.
-
+	int key,j;
+	for(int i = 1; i < N; i++){
+		key = d[i];
+		for(int k = 0; k < N; k++){
+			if (k==1) cout << "[" << d[k] << "]";
+			else cout << d[k] << " ";
+		}
+		j = i-1;			
+		pos = i;
+		while(j >= 0 && d[j] < key){
+			swap(d,j,j+1);
+			j--;
+		}
+		d[j+1] = key;
+		cout << " => ";
+		for(int k = 0; k < N; k++){
+			if (k==pos) cout << "[" << d[k] << "] ";
+			else cout << d[k] << " ";
+		}	
+		cout << "\n" ;
+	}
 }
 
 int main(){
